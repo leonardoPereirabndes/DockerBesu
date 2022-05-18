e3c98e52bf1d81a10a78e8c1479c5172659a9491
Leonardo <71888455+leonardoPereirabndes@users.noreply.github.com>
2022-05-18 16:05:41 -0300

    Update initialSetting.sh

--- NodeEnvironment/initialSetting.sh
+++ NodeEnvironment/initialSetting.sh
@@ -6,6 +6,6 @@ docker exec ${bootNode} besu --data-path=config  public-key export --to=./config
 chave=`docker exec ${validatorNode} cat ./config/nodeAddress`
 chave=`echo "${chave:2}\"]"`
 chave=`echo "[\"${chave}"`
-docker exec ${validatorNode} echo "${chave}" > ./NodeFile/ValidatorToExtraData.json
+echo "${chave}" > ./NodeFile/ValidatorToExtraData.json
 echo "extraData:"
 docker exec ${validatorNode} besu rlp encode --from=./config/ValidatorToExtraData.json
