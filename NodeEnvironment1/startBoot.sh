69d369105ef8beadc6e77fa1f96451e86ce5ef19
Leonardo <71888455+leonardoPereirabndes@users.noreply.github.com>
2022-05-26 15:00:22 -0300

    Update startBoot.sh

--- NodeEnvironment1/startBoot.sh
+++ NodeEnvironment1/startBoot.sh
@@ -10,5 +10,5 @@ do
  eNode=`echo "enode://${addressBoot:2}@${ipBoot}:30303 "`
  allENodeBoot=`echo "${allENodeBoot}${eNode}"`
 done
-
-docker exec ${entradaBoot}  besu --data-path=data --genesis-file=./config/genesis.json --config-file=./config/config.toml --rpc-http-enabled --rpc-http-api=ETH,ADMIN,PERM,NET,IBFT --host-allowlist="*" --rpc-http-cors-origins="all" --rpc-http-host="0.0.0.0"  --node-private-key-file=./config/key --rpc-http-port=8545 --bootnodes=${allENodeBoot}
+myIp=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${entradaBoot}`
+docker exec ${entradaBoot}  besu --data-path=data --genesis-file=./config/genesis.json --config-file=./config/config.toml  --p2p-host=${myIp} --rpc-http-enabled --rpc-http-api=ETH,ADMIN,PERM,NET,IBFT --host-allowlist="*" --rpc-http-cors-origins="all" --rpc-http-host="0.0.0.0"  --node-private-key-file=./config/key --rpc-http-port=8545 --bootnodes=${allENodeBoot}
